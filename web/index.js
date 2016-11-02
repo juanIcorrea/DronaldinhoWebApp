63654f9aae55cefbd0ae8035ac026411e99b497e
Juan Correa <juancorrea95@gmail.com>
2016-11-02 20:18:33 -0300

    Initial commit

---
+++ web/index.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Juan on 10/26/2016.
+ */
+var wsUri = "ws://10.10.10.5:8080";
+function run() {
+    output = document.getElementById("output");
+    start();
+}
+function takeOff() {
+    doSend("takeoff");
+
+}
+
+function down(){
+    doSend("down")
+}
+function left(){
+    doSend("left")
+}
+
+function right(){
+    doSend("right")
+}
+
+function forward(){
+    doSend("forward")
+}
+
+function backward(){
+    doSend("backward")
+}
+
+function rollLeft(){
+    doSend("rollLeft")
+}
+
+function rollRight(){
+    doSend("rollRight")
+}
+function up(){
+    doSend("up")
+}
+function start() {
+    alert("Started");
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) {
+        onOpen(evt)
+    };
+    websocket.onmessage = function(evt) {
+        onMessage(evt)
+    };
+    websocket.onerror = function(evt) {
+        onError(evt)
+    };
+}
+
+function onOpen(evt) {
+}
+function onMessage(evt) {
+    alert("recibo")
+}
+function onError(evt) {
+    //writeToScreen('ERROR: ' + evt.data);
+}
+function doSend(message) {
+    websocket.send(message);
+}
+
+
